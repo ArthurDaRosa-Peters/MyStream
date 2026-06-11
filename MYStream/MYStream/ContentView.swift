5da0308018cfb43babb8b97ee3083c621a012f86
delarossa <arthur.da_rosa-peters@edu.bib.de>
2026-06-11 10:48:02 +0200

    Add files via upload

---
+++ MYStream/MYStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MYStream
+//
+//  Created by Arthur da Rosa-Peters / PBD2H24A on 28.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MYStream/MYStream/MYStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYStreamApp.swift
+//  MYStream
+//
+//  Created by Arthur da Rosa-Peters / PBD2H24A on 28.05.26.
+//
+
+import SwiftUI
+
+@main
+struct MYStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
